37f3d441beacdd2d90bff7051b14f93dca54a3a3
sleepcha <sleepchaa@icloud.com>
2024-02-07 16:58:19 +0700

    Added `shouldInvalidateExpiredCache` option. Creation date is now saved instead of the date passed in `CacheMode.expires`. The process of validation based on creation date was extracted into a closure `isValid(Date) -> Bool`.

--- Sources/Fetchup/CacheMode.swift
+++ Sources/Fetchup/CacheMode.swift
@@ -3,8 +3,8 @@ import Foundation
 public enum CacheMode {
     /// Use the default caching mechanism with the policy set in `URLSessionConfiguration`
     case policy
-    /// Use a manual caching mode where all successful responses are cached (even those without cache-related headers). `Date` property specifies the expiration date.
-    case expires(Date)
+    /// Use a manual caching mode to cache every successful response (even those without cache-related headers).
+    case manual
     /// Prevent caching altogether.
     case disabled
 }
